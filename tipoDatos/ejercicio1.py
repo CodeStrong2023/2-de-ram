641473afdcf16f61d7ef119fb1529f7ab7e6de43
Gianni Pasquinelli <giannipasquinelli@gmail.com>
2023-09-05 09:20:33 -0300

    estructurando carpetas

--- tipoDatos/ejercicio1.py
+++
@@ -1,8 +0,0 @@
-#  Califica tu dia
-puntaje = input("¿Cómo estuvo tu día? (1 al 10): ")
-print("Mi día estuvo de", puntaje)
-
-#  ejercicio 2
-titulo = input("Proporciona el título:")
-autor = input("Proporciona el autor:")
-print(titulo, "fue escrito por", autor)
