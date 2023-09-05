862ba68195969d61139dc69f32f6ebd87793d460
Gianni Pasquinelli <giannipasquinelli@gmail.com>
2023-09-05 10:42:28 -0300

    clase 3 de javascript, tipos de datos

---
+++ JavaScript/Principio/index.js
@@ -0,0 +1,13 @@
+var nombre = 'Gianni'; // dato de tipo string
+var apellido = 'Pasquinelli';
+console.log(nombre + ' ' + apellido);
+
+var numero = 1234; // tipo numérico
+
+var obj = {
+  nombre: 'Belen',
+  apellido: 'Cortese',
+  telefono: '123123123',
+}; // tipo objeto
+
+console.log(obj);
