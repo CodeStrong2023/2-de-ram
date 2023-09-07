ad40f82aa18f6e1655d1de0da2d61272c77c5e9b
Luis Mera <luismera86@gmail.com>
2023-09-07 01:44:23 -0300

    Clase 3 Python - Complemento de set, ejercicio de diccionarios, pilas y colas

--- Luis Alejandro Mera/python/clase-02/set.py
+++ Luis Alejandro Mera/python/clase-02/set.py
@@ -1,4 +1,4 @@
-# Una colección de tipo set es una colección sin orden y sin indices
+# Una colección de tipo set es una colección sin orden y sin indices, solo tiene valore únicos por lo tanto no pueden tener duplicados
 
 planetas = {"Marte", "Júpiter", "Venus"}
 
@@ -38,5 +38,42 @@ print(planetas) # set()
 # Eliminar el set
 del planetas
 
+# Definir un set vació
+conjunto = set() 
 
+conjunto.add("Hola")
+conjunto.add(7)
+conjunto.add("Nombre")
+print(conjunto) # {'Nombre', 'Hola', 7}
+
+# Operaciones en set
+
+conjunto1 = {"Nombre", 44}
+
+# Unir dos set
+conjunto_unido = conjunto | conjunto1
+print(conjunto_unido) # {'Nombre', 'Hola', 44, 7} No se suman los elementos repetidos solo se muestra uno
+
+# Obtener los elementos en común que tienen los conjuntos
+conjunto_comun = conjunto & conjunto1
+print(conjunto_comun) # {'Nombre'}
+
+# Obtener los elementos que no están en otro conjunto
+conjunto_diferentes = conjunto - conjunto1 # Almacenamos los elementos de conjunto que no están en conjunto1
+print(conjunto_diferentes) # {'Hola', 7}
+
+# Obtener los elementos que no comparten entre ambos conjuntos
+conjunto_no_compartidos = conjunto ^ conjunto1
+print(conjunto_no_compartidos) # {44, 7, 'Hola'}
+
+# Como saber si un conjunto pertenece a toro conjunto 
+print(conjunto.issubset(conjunto1)) # False
+print(conjunto.issubset(conjunto_unido)) # True Porque es una union de conjunto y conjunto1
+
+# Como saber si un conjunto contiene otro conjunto
+print(conjunto_unido.issuperset(conjunto)) # True
+print(conjunto_unido.issuperset(conjunto1)) # True
+
+# Convertir un set en inmutable
+conjunto = frozenset
 
