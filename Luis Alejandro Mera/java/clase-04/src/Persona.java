c8d46413a9a0635371c453282ab655bd0a6446b8
Luis Mera <luismera86@gmail.com>
2023-09-14 20:50:02 -0300

    Clase 4 Java: Objetos persona y ejercicio 6 ciclos

---
+++ Luis Alejandro Mera/java/clase-04/src/Persona.java
@@ -0,0 +1,11 @@
+public class Persona {
+    // Atributos de las clases(Caracteristicas)
+    String nombre;
+    String apellido;
+
+    // Métodos de ls clases (Acciones)
+    public void obtenerInformacion(){
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Apellido: " + apellido);
+    }
+}
