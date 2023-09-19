c4fc88d7885dbb7981469d05a404bbab2cc53a86
Luis Mera <luismera86@gmail.com>
2023-09-18 22:08:59 -0300

    .

--- Luis Alejandro Mera/python/clase-05/ejercicios/ejercicio05.py
+++ Luis Alejandro Mera/python/clase-05/ejercicios/ejercicio05.py
@@ -5,7 +5,7 @@ while numero < 0:
     print("Error -> El número tiene que ser positivo")
     numero = int(input("Digite un número: "))
 
-factorial = 0
+factorial = 1
 
 for i in range(1, numero+1):
     factorial += i
