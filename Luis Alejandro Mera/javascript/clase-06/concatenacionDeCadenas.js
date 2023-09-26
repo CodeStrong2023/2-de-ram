aa0a3cb4d1017f9e4fba521022f666c8e6c47aef
Luis Mera <luismera86@gmail.com>
2023-09-25 22:34:05 -0300

    Clase 6 de JavaScript: Concatenación de cadenas

---
+++ Luis Alejandro Mera/javascript/clase-06/concatenacionDeCadenas.js
@@ -0,0 +1,24 @@
+
+var nombre = "Jose";
+var apellido = " Montes";
+var nombreCompleto = nombre + " " + apellido
+
+console.log(nombreCompleto);
+
+var nombreCompleto2 = "Juan" + " " + " Perez";
+console.log(nombreCompleto2);
+
+var juntos = nombre + 244
+console.log(juntos);
+
+juntos = nombre + 78 + 17;
+console.log(juntos);
+
+juntos = 78 + 17 + nombre;
+console.log(juntos);
+
+nombre += apellido
+console.log(nombre);
+
+
+
