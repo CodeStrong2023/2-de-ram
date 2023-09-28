17401831764eaebe3d91a0ca811cfa11609b0c85
juan lara <136129133+juanlara23@users.noreply.github.com>
2023-09-28 15:57:56 -0300

    python
    ejercicios de la clase de python

---
+++ juan lara/python/calse 1 python/ejercisios.py
@@ -0,0 +1,13 @@
+print("Ejercisio 1: Rango de 0 a 10 con numero divisibles entre 3")
+for i in range(11):
+    if i % 3 == 0:
+        print(i)
+        
+print("Ejercisio 2: rango con valores de inicio = 2 y fin = 6")
+rango = range(2, 7)
+for i in rango:
+    print(i)
+    
+print("Ejercisio 3: Crear un rango de 3 a 10, con incremento de 2 en 3")
+for i in range(3, 11, 2):
+    print(i)
