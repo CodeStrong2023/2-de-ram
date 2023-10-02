ae3181d94e36e015056ddf253039e9340babea8f
apesalaccia <apesalaccia@homtial.com>
2023-10-02 18:38:20 -0300

    update

--- Alan Pesalaccia/Java/Clase3/Clase3/src/clase3/Clase3.java
+++ Alan Pesalaccia/Java/Clase3/Clase3/src/clase3/Clase3.java
@@ -1,20 +1,19 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+//Ejercicio3: leer numeros hasta que se introduzca un cero
+//para cada uno indicar si es par o impar
 package clase3;
 
-/**
- *
- * @author apesa
- */
+import java.util.Scanner;
+
+
 public class Clase3 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+        int numero;
+        System.out.println("Digite un numero: ");
+        numero = Integer.parseInt(sc.nextLine());
+        
+        
     }
     
 }
