bf89adf7e39b784aafa00cca67be4e25b3a8e93f
Ignacio Dahuc <108817856+nachojd@users.noreply.github.com>
2023-10-03 03:18:19 -0300

    Delete Luis Alejandro Mera directory

--- Luis Alejandro Mera/python/clase-06/funciones/paso-de-argumentos.py
+++
@@ -1,9 +0,0 @@
-
-
-# Paso de argumentos
-
-def mi_funcion(name, lastName):
-    print(f"Nombre: {name}, Apellido: {lastName}")
-
-mi_funcion("Juan", "Perez")
-mi_funcion("Pedro", "Aguirre")
--- Luis Alejandro Mera/python/clase-06/funciones/valores-default-aparametros.py
+++
@@ -1,9 +0,0 @@
-
-# Valores por default en los parámetros
-
-def sumar(a = 0, b = 0): # Se le asigna el valor por default si no se le ingresa ningún argumento
-    return a + b
-
-resultado = sumar()
-
-print(resultado)
