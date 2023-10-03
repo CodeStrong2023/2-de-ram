07cdc1021481f1b50438aafcca3eaea0418546fe
Gianni Pasquinelli <giannipasquinelli@gmail.com>
2023-10-03 09:08:42 -0300

    clase 7 javascript

---
+++ Gianni Pasquinelli/JavaScript/clase07/index.js
@@ -0,0 +1,19 @@
+//  encontrar numeros pares
+
+let parImpar = 10;
+if (parImpar % 2 == 0) {
+    console.log("Es par");
+} else {
+    console.log("Es impar");
+}
+
+
+// mayor de edad
+let edad = 20;
+let adulto = 18;
+
+if (edad >= adulto) {
+    console.log("Es mayor de edad");
+} else {  
+    console.log("Es menor de edad");
+}
