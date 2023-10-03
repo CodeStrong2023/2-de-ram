2fd5e2e89517f218f32fce1e399e8ea604681f6f
Ignacio Dahuc <108817856+nachojd@users.noreply.github.com>
2023-10-03 03:19:20 -0300

    Delete juan lara directory

--- juan lara/java ejercicios/java ejercicios/cicloDoWhile.java
+++
@@ -1,5 +0,0 @@
-public class cicloDoWhile {
-    public static void main(String[] args) {
-        
-    }
-}
--- juan lara/python/clase 2 python/tuplas2.py
+++
@@ -1,8 +0,0 @@
-# Podemos colocar todo tipo de dato en las tuplas
-
-tupla = (4, 'River', 10.0, [1, 2, 3], 4, 'River')
-print(tupla)
-
-# Podemos realizar una BUSQUEDA DE UN TIPO y nos devuelve un tipo booleano (true or false)
-
-print(4 in tupla)
