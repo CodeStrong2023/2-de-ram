d628d06752fa6c4c201d11f5b9b10f7ed249464f
Ignacio Dahuc <ignaciodahuc12@gmail.com>
2023-10-03 02:50:54 -0300

    Programación II - Clase 5

--- Luis Alejandro Mera/python/clase-05/ejercicios/ejercicio04.py
+++
@@ -1,9 +0,0 @@
-
-a = int(input("Digite de donde va a comenzar la suma: "))
-b = int(input("Digite hasta donde va a comenzar la suma: "))
-suma = 0
-for i in range(a,b+1):
-    if i % 2 == 0:
-        suma += i
-
-print(f"\nLa suma de números pares dentro del rango es: {suma}")
--- Luis Alejandro Mera/python/clase-05/ejercicios/ejercicio06.py
+++
@@ -1,6 +0,0 @@
-
-numero = int(input("Digite un número: "))
-
-for i in range(1, 11):
-    resultado = numero * i
-    print(f"{numero} X {i} = {resultado}")
