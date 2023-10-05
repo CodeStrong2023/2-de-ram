3b6d7865e66b6cd47587bc7501be26f94bed73af
ArtiedaLorena <artiedalorena@gmail.com>
2023-10-05 14:36:45 -0300

    Clase 7 Programacion II

--- Lorena Artieda/Java/Leccion04/Aritmetica/src/Operaciones/PruebaAritmetica.java
+++ Lorena Artieda/Java/Leccion04/Aritmetica/src/Operaciones/PruebaAritmetica.java
@@ -18,6 +18,11 @@ public class PruebaAritmetica {
         System.out.println("El resultado es: "+resultado2);
 
         Aritmetica aritmetica2= new Aritmetica(3,6);
+
+        Persona persona1= new Persona("Ariel", "Betancud");
+        System.out.println("persona1 = " + persona1);
+        System.out.println("Persona nombre: "+ persona1.nombre);
+        System.out.println("Persona apellido: "+ persona1.apellido);
     }
 
    public static void miMetodo(){
@@ -25,4 +30,33 @@ public class PruebaAritmetica {
         //b=12;
        System.out.println("Aqui hay otro metodo");
    }
+}
+
+//Clase default o package(Solo puede haber una clase publica)
+
+class Persona{
+    String nombre;
+    String apellido;
+
+    Persona(String nombre, String apellido){
+
+        super(); //Llamado al contructor padre object
+        //Imprimir imprimir= new Imprimir();
+        new Imprimir().imprimir(this);
+        this.nombre=nombre;
+        this.apellido=apellido;
+        System.out.println("Objeto persona usando this: "+ this);
+
+    }
+
+}
+
+class Imprimir{
+    public Imprimir(){
+        super();//El contructor de la clase padre para reservar memoria
+    }
+    public void imprimir(Persona persona){
+        System.out.println("Persona desde la clase imprimir: "+ persona);
+        System.out.println("Impresion del objeto actual (this): "+ this);
+    }
 }
