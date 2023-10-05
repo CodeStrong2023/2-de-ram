743dd7f58c76cbf20919d567e90df21a12c609a7
Luis Mera <luismera86@gmail.com>
2023-10-04 21:58:56 -0300

    Python clase 7: ejercicio 12 funciones con argumentos

---
+++ Luis Alejandro Mera/python/clase-07/ejercicios/ejercicio12.py
@@ -0,0 +1,12 @@
+
+# Función con argumentos para multiplicar
+
+# Definimos la función para multiplicar
+
+def multiplicar_valores(*numeros):
+    resultado = 1
+    for numero in numeros:
+        resultado *= numero
+    return resultado
+
+print(multiplicar_valores(3,5,15,50,4))
