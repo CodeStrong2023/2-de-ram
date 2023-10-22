7c4026581ab17abde88232c9cc92dc1e9856cf6e
santiago villarreal <santivilla795@hotmail.com>
2023-10-22 18:47:21 -0300

    Clase 6 JavaScript

---
+++ Santiago-Villarreal/JavaScript/2/02-01-ConcatenacionDeCadenas.js
@@ -0,0 +1,15 @@
+var nombre = 'Jose';
+var apellido = 'Montes';
+var nombreCompleto = nombre + ' ' + apellido;
+console.log(nombreCompleto);
+var nombreCompleto2 = 'Ariel' + ' ' + 'Betancud';
+console.log(nombreCompleto2);
+var juntos = nombre + 219;
+console.log(juntos)
+juntos = nombre + 78 + 17;
+console.log(juntos)
+juntos = 78 + 17 + nombre;
+console.log(juntos);
+
+nombre += apellido;
+console.log(nombre)
