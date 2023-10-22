ac91c85ea9ae4238552af94c8b2b8c4fe6b6ead3
Luis Mera <luismera86@gmail.com>
2023-10-21 21:49:39 -0300

    Clase 10 Java: paquetes y palabra reservada final

---
+++ Luis Alejandro Mera/java/src/clase10/paquetes/src/test/TestUtileria.java
@@ -0,0 +1,13 @@
+package clase10.paquetes.src.test;
+
+import clase10.paquetes.src.ar.com.codesystem.Utileria;
+//import static clase10.paquetes.src.ar.com.codesystem.Utileria.imprimir;
+
+public class TestUtileria {
+
+    public static void main(String[] args) {
+        Utileria.imprimir("Hola saludos");
+//        imprimir("Otra sintaxis de importación de paquetes"); (No recomendada)
+//        clase10.paquetes.src.ar.com.codesystem.Utileria.imprimir("Importando directamente el paquete"); (No recomendada)
+    }
+}
