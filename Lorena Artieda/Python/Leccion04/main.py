c30d6aebd7f58b0ffe680b41812fa2751f6e6d21
Lorena <artiedalorena@gmail.com>
2023-10-27 15:58:08 -0300

    Segunda calse de Python

--- Lorena Artieda/Python/Leccion04/main.py
+++ Lorena Artieda/Python/Leccion04/main.py
@@ -1,4 +1,7 @@
 #Listas: conjunto de elementos
+#Colecciones en python
+
+#Las listas es lo que se conoce en otros lengujaes como arreglos o vectores
 
 nombres=['Natalia', 'Ariel', 'Osvaldo', 'Liliana']
 print(nombres)
@@ -24,6 +27,11 @@ print(len(nombres))#Le pasamos como parametro la lista
 
 #Agregamso un elemento
 nombres.append(("Marcelo"))
+nombres.append([1,2,3])
+nombres.append((True))
+nombres.append(10.45)
+nombres.append([4,5])
+nombres.append(7)
 print(nombres)
 
 #Insertar un elemento en un indice especifico
@@ -66,8 +74,137 @@ cocinaLista[0]='plato'
 cocina= tuple(cocinaLista)
 print('\n', cocina)
 
-#del cocina
+#del cocina / Para eliminar una tupla
 print(cocina)
+
+#TIPO SET(o conjunto)
+#Un elemento de tipo set es un elemento sin orden que no mantiene ningun indice
+planetas= {'Marte', 'Júpiter','Venus' }
+print(len(planetas))
+
+#Revisar si un elemento existe dentro de set
+print('Martes' in planetas)
+
+#Agregar un elemento
+planetas.add('Tierra')#Add es una funcion
+print(planetas)
+#Eliminar elementos, puede arrojar un error si el elemento no existe
+planetas.remove('Júpiter')
+print(planetas)
+planetas.discard('tierra')
+print(planetas)
+
+#Limpiar set
+planetas.clear()
+print(planetas)
+
+#Eliminar set
+del planetas
+#print(planetas)
+
+#'Maradona' :10 UN DICCIONARIO ESTA COMPUESTO POR DOS ELEMENTOS
+#UNA LLAVE Y UN VALOR
+#dict(key,value)
+
+diccionario={
+    'IDE': "Integrated Development Environment",
+    'POO': 'Programacion Orientada a Objetos',
+    'SABD':'Sistema de administracion de Base de Datos'
+}
+
+print(diccionario)
+
+#Verificar la cantidad de elementos de ldiccionario
+print(len(diccionario))
+print(diccionario)
+
+#Acceder a un diccionario con la llave
+print(diccionario['IDE'])
+
+#Otra forma de recuperar un elemento
+print(diccionario.get('POO'))
+print(diccionario.get('SABD'))
+
+#Modificamos elementos
+diccionario['IDE']= "Entorno de desarrollo integrado"
+print(diccionario)
+
+#Como recorrer los elementos
+for termino in diccionario:
+    print(termino)
+
+#Necesitamos una funcion para recorrer un diccionario
+for termino, valor in diccionario.items():
+    print(termino,valor)
+
+#Otras maneras de acceder a un diccionario
+for termino in diccionario.keys():
+    print(termino)
+
+for valor in diccionario.values():
+    print(valor)
+
+#Comprobar la existencia de algun elemento
+print('IDE' in diccionario)#Devuelve un booleano
+
+#Agregar un elemento
+diccionario['PK'] = 'Primary Key'
+print(diccionario)
+
+#Eliminar un elementp
+diccionario.pop('SABD')
+print(diccionario)
+
+#Vaciar un diccionario
+diccionario.clear()
+print(diccionario)
+
+#Eliminar diccionario
+del diccionario
+
+#Concatenamos Listas
+lista1= [1,2,3]
+lista2=[4,5,6]
+lista3= lista1+lista2
+print(lista3)
+
+lista3.extend([7,8,9,1])
+print(lista3)
+
+print(lista3.index(5))#Fncion para ubicar en que indice esta el valor ingresado
+#Como saber cuantos valores repetidos hay dentro de una lista
+print(lista3.count(1))
+
+#Para poner al reves la lista
+lista3.reverse()
+print(lista3)
+
+#Para que una lista se multiplique utilizando sus elementos
+lista = [1,2,3] * 2
+print(lista3)
+
+#Metodos de ordenamiento
+lista3.sort()
+print(lista3)
+lista3.sort(reverse=True)
+print(lista3)
+
+#Repaso tuplas
+
+tupla= (4,'Hola',6.78,[1,2,78], 4, 'Hola')
+print(tupla)
+print(4 in tupla)#Accion Booleanda
+
+
+
+
+
+
+
+
+
+
+
 
 
 
