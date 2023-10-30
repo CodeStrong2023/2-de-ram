4f93c2904d73ccd64384cf7d840a729be75023de
Lorena <artiedalorena@gmail.com>
2023-10-30 15:09:22 -0300

    Cuarta clase Python

--- Lorena Artieda/Python/Leccion04/EjercicioTuplas.py
+++ Lorena Artieda/Python/Leccion04/EjercicioTuplas.py
@@ -1,3 +1,4 @@
+import math
 #Dada ña siguiente tupla
 tupla=(13,1,8,3,2,5,8)
 #Crear una lista quw solo incluya los numneros menores a 5
@@ -13,3 +14,11 @@ for numero in tupla:
 
 # Imprime la lista_resultado por consola.
 print(lista_resultado)
+
+#Ejercicio de matematicas
+#Para sacar la raiz cuadrada de un numero positivo
+numero= int(input('Digite un numero positivo: '))
+while numero<0:
+    print('Error -> Deberia ser un numero positivo')
+    numero= int(input('Digite un numero positivo: '))
+print(f'\nSu raiz cuadrada es: {math.sqrt(numero):.2f}')
