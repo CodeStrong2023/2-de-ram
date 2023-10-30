4a38556707b4671a6b824f3a0210ee2ca8ad73d1
santiago villarreal <santivilla795@hotmail.com>
2023-10-29 21:40:30 -0300

    Java clase 7 2s

---
+++ Santiago-Villarreal/Java/2s/leccionClasesObjetos/src/clases/Persona (2).java
@@ -0,0 +1,13 @@
+package clases;
+
+public class Persona {
+    // Atributtos de la clase
+    public String nombre;
+    public String apellido;
+    // Métodos de la clase
+    public void obtenerInformacion(){
+        System.out.println("Nombre = " + nombre);
+        System.out.println("Apellido = " + apellido);
+        
+    }
+}
