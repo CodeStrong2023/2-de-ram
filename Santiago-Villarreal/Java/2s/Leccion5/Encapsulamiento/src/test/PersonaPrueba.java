9f4bd2cf5912d24bdae888eed329bdce4c29cdfa
Santiago Villarreal <santivilla795@hotmail.com>
2023-11-06 20:46:05 -0300

    java clase 8 completa

--- Santiago-Villarreal/Java/2s/Leccion5/Encapsulamiento/src/test/PersonaPrueba.java
+++ Santiago-Villarreal/Java/2s/Leccion5/Encapsulamiento/src/test/PersonaPrueba.java
@@ -6,6 +6,7 @@ import Dominio.Persona;
 public class PersonaPrueba {
     public static void main(String[] args) {
         Persona persona1 = new Persona("Osvaldo",57.000,false);
+        System.out.println("persona1: " + persona1.toString());
         System.out.println("persona1 su nombre es: " + persona1.getNombre());
         //modificar a travez del método
         persona1.setNombre("Juan Ignacio");
@@ -14,6 +15,7 @@ public class PersonaPrueba {
         System.out.println("persona1 el resultado para el sueldo: " + persona1.getSueldo());
         System.out.println("persona1 para obtener el booleano: " + persona1.isEliminado());
         
+        System.out.println("persona1: " + persona1.toString());
         
     }
 }
