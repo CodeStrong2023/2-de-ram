08cc6d017e7cf39f497f8e7c5d96fd1b77511692
apesalaccia <apesalaccia@homtial.com>
2023-11-08 21:24:04 -0300

    JavaClase11

---
+++ Alan Pesalaccia/Java/Clase11/Arreglos/src/arreglos/TestArreglos.java
@@ -0,0 +1,16 @@
+
+package arreglos;
+
+public class TestArreglos {
+
+
+    public static void main(String[] args) { //lado derecho instanciamos un obj de tipo object
+        int edades[] = new int [3]; //el lado izq declaramos la variable
+        System.out.println("edades = " + edades);
+        
+        edades[0] = 17;
+        System.out.println("edades 0 = " + edades[0]);
+
+    }
+    
+}
