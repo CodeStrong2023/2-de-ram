fc321c082ede0ef2c67c448d1e07e6b978fa319c
nachojd <ignaciodahuc12@gmail.com>
2023-11-10 19:35:42 -0300

    Laboratorio II - Clase 10

---
+++ Ignacio Dahuc/Python/Lección6/PruebaPersona2.py
@@ -0,0 +1,10 @@
+from Persona2 import Persona2
+print(" Creación de objetos ".center(50, "-"))
+persona5 = Persona2("Juan", "Perez", 32)
+persona5.mostrar_detalle()
+
+print(__name__) # Esto nos muestra el nombre del módulo que estamos ejecutando
+
+print(" Eliminación de objetos ".center(50, "-"))
+
+del persona5 # Esto nos permite eliminar el objeto de la memoria con el método destructor __del__ de la clase Persona2
