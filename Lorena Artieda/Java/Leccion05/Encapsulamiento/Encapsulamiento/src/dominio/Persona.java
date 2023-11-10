789bd7ec81767bb9c6707825f9a1ee02984da1eb
Lorena <artiedalorena@gmail.com>
2023-11-10 11:18:37 -0300

    Clase 10 y 11 de Java

--- Lorena Artieda/Java/Leccion05/Encapsulamiento/Encapsulamiento/src/dominio/Persona.java
+++ Lorena Artieda/Java/Leccion05/Encapsulamiento/Encapsulamiento/src/dominio/Persona.java
@@ -2,16 +2,27 @@ package dominio;
 
 public class Persona {
     //Atributos
+    private int idPersona;
+    private static int contadorPersona;
     private String nombre;
-    private double sueldo;
-    private boolean eliminado;
+    //private double sueldo;
+    //private boolean eliminado;
 
     //Constructor
-    public Persona(String nombre, double sueldo, boolean eliminado) {
+
+    public Persona(String nombre) {
         this.nombre = nombre;
-        this.sueldo = sueldo;
-        this.eliminado = eliminado;
+        //Incrementar el contador por cada objeto nuevo
+        Persona.contadorPersona++;
+        //Vamos a asignar un nuevo valor a la variable id persona
+        this.idPersona= Persona.contadorPersona;
     }
+
+    /*public Persona(String nombre) {
+        this.nombre = nombre;
+        //this.sueldo = sueldo;
+        //this.eliminado = eliminado;
+    }*/
 
     public String getNombre() {
         return nombre;
@@ -21,10 +32,12 @@ public class Persona {
         this.nombre = nombre;
     }
 
-    public double getSueldo() {
+    /*public double getSueldo() {
         return sueldo;
     }
 
+     */
+    /*
     public void setSueldo(double sueldo) {
         this.sueldo = sueldo;
     }
@@ -36,4 +49,39 @@ public class Persona {
     public void setEliminado(boolean eliminado) {
         this.eliminado = eliminado;
     }
+
+    @Override
+    public String toString() {
+        return "Persona{" +
+                "nombre='" + nombre + '\'' +
+                ", sueldo=" + sueldo +
+                ", eliminado=" + eliminado +
+                '}';
+        */
+
+    public int getIdPersona() {
+        return idPersona;
+    }
+
+    public void setIdPersona(int idPersona) {
+        this.idPersona = idPersona;
+    }
+
+    public static int getContadorPersona() {
+        return contadorPersona;
+    }
+
+    public static void setContadorPersona(int contadorPersona) {
+        Persona.contadorPersona = contadorPersona;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" +
+                "idPersona=" + idPersona +
+                ", nombre='" + nombre + '\'' +
+                '}';
+    }
 }
+
+
---
+++ Lorena Artieda/Java/Leccion06/Herencia/src/test/TestPersona.java
@@ -0,0 +1,17 @@
+package test;
+
+import domain.Persona;
+
+public class TestPersona {
+    public static void main(String[] args) {
+        Persona persona1 = new Persona("Luis");
+        System.out.println("persona1 = " + persona1);
+        Persona persona2 = new Persona("Juan");
+        System.out.println("persona2 = " + persona2);
+
+        imprimir(persona1);
+    }
+    public static void imprimir(Persona persona) {
+        System.out.println("persona = " + persona);
+    }
+}
