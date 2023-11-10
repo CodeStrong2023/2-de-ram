b77f85193f37230a439df67f6860ffdaf3d71080
nachojd <ignaciodahuc12@gmail.com>
2023-11-10 19:29:15 -0300

    Laboratorio II - Clase 9

--- Ignacio Dahuc/JavaScript/leccion03/03-01-OperadoresJavaScript-UJS.js
+++ Ignacio Dahuc/JavaScript/leccion03/03-01-OperadoresJavaScript-UJS.js
@@ -27,3 +27,44 @@ if(dentroRango >= valMin && dentroRango <= valMax){
 else{
     console.log('Esta fuera del rango establecido');
 }
+
+// Ejercicio: si el padre puede asistir al juego de su hijo
+let vacaciones = false;
+let diaDescanso = false;
+
+if (vacaciones || diaDescanso) {
+  console.log("El padre puede asistir al juego de su hijo");
+} else {
+  console.log("El padre puede asistir al juego de su hijo");
+}
+
+// Operador ternario 
+let resultado2 = vacaciones || diaDescanso ? "El padre puede asistir al juego de su hijo" : "El padre puede asistir al juego de su hijo";
+
+// Operador ternario 
+let resultado = 3 > 2 ? "Verdadero" : "Falso";
+console.log(resultado);
+
+// Convertir string a number
+let miNumero = "12"; 
+console.log(typeof miNumero);
+
+let edad1 = Number(miNumero); // Función Number() tranforma los string a tipo number
+console.log(typeof edad);
+
+if(edad >= 18) {
+    console.log("Puede votar");
+} else {
+    console.log("No puede votar");
+}
+
+let resultado3 = edad >= 18 ? "Puede votar" : "No puede votar";
+console.log(resultado2);
+
+// Función isNaN
+let edad2 = "10x"
+if(isNaN(edad2)) { // El número puede venir en un string, solo verifica que no tenga letras
+    console.log("La variable edad2 no son solo números");
+} else {
+    console.log("La variable edad2 es un número");
+}
--- Ignacio Dahuc/Python/Lección6/Persona.py
+++ Ignacio Dahuc/Python/Lección6/Persona.py
@@ -25,6 +25,15 @@ print(f'El objeto 1 modificado de la clase persona: {persona1.nombre} {persona1.
 
 # Los atributos son: caracteristicas
 # Los métodos son: el comportamiento que van a tener los objetos (acciones)
-persona1.mostrar_detalle()
+persona1.mostrar_detalle() # La referencia en este caso se pasa de manera automatica
 persona2.mostrar_detalle()
 
+# Persona.mostrar_detalle(persona1) # Debemos pasarle una referencia para el self o dará error
+
+# Crear un atributo para un objeto específico
+persona1.telefono = "156242"
+print(f"Este es el teléfono {persona1.telefono} de {persona1.nombre}")
+
+persona3 = Persona("Juan", "Perez", 55124, 32, "Colon", 1113, "Barrio 14", Altura=1.71, Peso=63, Auto="Peugeot 206")
+print(persona3.mostrar_detalle())
+# print(persona3._dni) no se puede utilizar para encapsulamiento
